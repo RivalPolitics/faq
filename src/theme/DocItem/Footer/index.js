53f7a58de17ed3b5cbf293171980b3a02667d61a
Riva Politics <developer@rivalpolitics.com>
2024-11-07 11:35:03 +0300

    add footer docs

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';
-import TagsListInline from '@theme/TagsListInline';
-import EditMetaRow from '@theme/EditMetaRow';
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
+import TagsListInline from "@theme/TagsListInline";
+import EditMetaRow from "@theme/EditMetaRow";
 export default function DocItemFooter() {
-  const {metadata} = useDoc();
-  const {editUrl, lastUpdatedAt, lastUpdatedBy, tags} = metadata;
+  const { metadata } = useDoc();
+  const { editUrl, lastUpdatedAt, lastUpdatedBy, tags } = metadata;
   const canDisplayTagsRow = tags.length > 0;
   const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
   const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
@@ -15,8 +15,27 @@ export default function DocItemFooter() {
   }
   return (
     <footer
-      className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
-      {canDisplayTagsRow && (
+      className={clsx(ThemeClassNames.docs.docFooter, "docusaurus-mt-lg")}
+    >
+      <div className="text-[15px] border-t-[1px] border-solid border-gray-300 border-x-0 border-b-0 mt-3 pt-3 text-gray-600 pl-2">
+        С уважением, администрация Rival Politics &#x2022;{" "}
+        <a href="https://github.com/RivalPolitics/faq/issues/new">
+          по любым ошибкам сообщите сюда
+        </a>
+        {canDisplayTagsRow && (
+          <div
+            className={clsx(
+              "row margin-top--sm",
+              ThemeClassNames.docs.docFooterTagsRow
+            )}
+          >
+            <div className="col">
+              <TagsListInline tags={tags} />
+            </div>
+          </div>
+        )}
+      </div>
+      {/*{canDisplayTagsRow && (
         <div
           className={clsx(
             'row margin-top--sm',
@@ -37,7 +56,7 @@ export default function DocItemFooter() {
           lastUpdatedAt={lastUpdatedAt}
           lastUpdatedBy={lastUpdatedBy}
         />
-      )}
+      )}*/}
     </footer>
   );
 }
